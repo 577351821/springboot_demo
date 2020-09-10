a4e14751d71b1b86cb5a991e4d55860fcee8092d
huang <577351821@qq.com>
2020-09-10 22:52:12 +0800

    添加abc222111

---
+++ springboot_demo/src/main/java/cn/itcast/bean/test.java
@@ -0,0 +1,12 @@
+package cn.itcast.bean;
+
+/**
+ * @description:
+ * @author: huang
+ * @create: 2020-09-10 22:51
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
